91a4ef2aa89b12e31003a612765f9056b5239fb0
Aleksandr Yarchuk <learnmeasalex1912@ukr.net>
2020-04-05 18:39:34 +0300

    wtf commit

---
+++ src/main/java/ButtonsHolder.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ButtonsHolder {
+
+    public void buttonHolder(Message message) {
+
+    }
+
+}
+
---
+++ src/main/java/ButtonsSettings.java
@@ -0,0 +1,11 @@
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
+public class ButtonsSettings {
+
+    public static void getSettings(ReplyKeyboardMarkup replyKeyboardMarkup) {
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+    }
+
+}
