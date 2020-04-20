cd2b0885ef0b94ff2c3118154bb3c452913816da
Aleksandr Yarchuk <learnmeasalex1912@ukr.net>
2020-04-20 17:53:31 +0300

    fixed time zone error (fuck u heroku)

--- src/main/java/workWithTime/CheckSchedule.java
+++ src/main/java/workWithTime/CheckSchedule.java
@@ -5,6 +5,8 @@ import entity.User;
 import service.UserService;
 
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.TimerTask;
@@ -14,13 +16,17 @@ public class CheckSchedule extends TimerTask {
     private final AssistantBot bot = new AssistantBot();
     private final UserService userService = new UserService();
 
+    private final ZoneId kievZoneId = ZoneId.of("Europe/Kiev");
+
+
     @Override
     public void run() {
-
         List<User> userList = userService.findAllUsers();
 
+        System.out.println(userList + " СМОТРИ СЮДА БЛЯЯЯЯЯЯЯЯЯТЬ");
+
         for (User user : userList) {
-            if (LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm")).equals(user.getScheduleTime())) {
+            if (LocalTime.now(kievZoneId).format(DateTimeFormatter.ofPattern("HH:mm")).equals(user.getScheduleTime())) {
                 bot.sendSchedule(user);
             }
         }
